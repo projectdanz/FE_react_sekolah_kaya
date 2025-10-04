ff93936468736b0c37194f6ee581cc6749c7354e
projectdanz <akunprojectdanz@gmail.com>
2025-10-04 13:23:32 +0700

    chore: set up to page course detail

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from './pages/mainDashboard';
+// import CourseDetail from './pages/courseDetail';
 
 createRoot(document.getElementById('root')).render(
   //  <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route path="/courses/:slug" element={<CourseDetail />} /> */}
       </Routes>
     </BrowserRouter>
   // </React.StrictMode>
