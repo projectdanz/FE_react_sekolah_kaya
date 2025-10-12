2bee16c7d04a1d12feed9b655a189a6a36a33d82
projectdanz <akunprojectdanz@gmail.com>
2025-10-12 11:36:02 +0700

    chore: set up the Wi-Fi IP for access via the same Wi-Fi

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 5000,
+    host: "172.20.1.172",
+  },
   plugins: [
     react(),
     tailwindcss()
