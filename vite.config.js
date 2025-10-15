aa12d2d7ec7453fa4bcbf0e10a28332f80a839a3
projectdanz <akunprojectdanz@gmail.com>
2025-10-15 07:19:48 +0700

    fix: server improvements and additional set-up for deployment on Vercel

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 5000,
-    host: "172.20.1.172",
+    port: 5000
+    // host: "172.20.1.172",
   },
   plugins: [
     react(),
